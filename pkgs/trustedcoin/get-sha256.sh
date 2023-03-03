0f1191200d0bb51294f3cf3cee20aeb68b596825
Otto Sabart <seberm@seberm.com>
2023-03-03 21:00:00 +0100

    trustedcoin: fix shellcheck

--- pkgs/trustedcoin/get-sha256.sh
+++ pkgs/trustedcoin/get-sha256.sh
@@ -3,17 +3,17 @@
 set -euo pipefail
 
 TMPDIR="$(mktemp -d -p /tmp)"
-trap "rm -rf $TMPDIR" EXIT
-cd $TMPDIR
+trap 'rm -rf $TMPDIR' EXIT
+cd "$TMPDIR"
 
 echo "Fetching latest release"
 repo=fiatjaf/trustedcoin
-latest=$(curl -s --show-error https://api.github.com/repos/$repo/releases/latest | jq -r .tag_name)
+latest=$(curl -s --show-error "https://api.github.com/repos/${repo}/releases/latest" | jq -r .tag_name)
 echo "Latest release is $latest"
-git clone --depth 1 --branch $latest https://github.com/fiatjaf/trustedcoin 2>/dev/null
+git clone --depth 1 --branch "$latest" https://github.com/fiatjaf/trustedcoin 2>/dev/null
 cd trustedcoin
 
 echo "tag: $latest"
-git checkout -q tags/$latest
+git checkout -q "tags/${latest}"
 rm -rf .git
 nix --extra-experimental-features nix-command hash path .
